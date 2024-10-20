d8eebc8e05beb45b0e3dcc288e71282b5b24345f
Rashid <rashidgaitov@gmail.com>
2024-10-20 21:23:13 +0300

    Added movement on WASD keys

--- Unity Entry Project/Assets/Scripts/ForceApplication.cs
+++ Unity Entry Project/Assets/Scripts/ForceApplication.cs
@@ -16,11 +16,21 @@ public class ForceApplication : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        upplyForce();
-        
+        move();
     }
 
-    void upplyForce() {
-        rb.AddForce(Vector3.forward * force, ForceMode.Impulse);
+    void move() {
+        if (Input.GetKey(KeyCode.W)) {
+            rb.AddForce(Vector3.forward * force, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            rb.AddForce(Vector3.left * force, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            rb.AddForce(Vector3.back * force, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            rb.AddForce(Vector3.right * force, ForceMode.Impulse);
+        }
     }
 }
